a6ce459a610274bde4f640c80ff55b6c4dcb5193
Aditya yadav <157573310+Aadityaa-11@users.noreply.github.com>
2025-11-15 20:06:04 +0530

    Time: 0 ms (100%), Space: 21.8 MB (37.55%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -11,42 +11,42 @@ public:
     //     return nums.size();
     // }
 
-    int brute_force2(vector<int>& nums){
+    // int brute_force2(vector<int>& nums){
 
-        sort(nums.begin() , nums.end());
-        int ans = -1;
-        int diff = 1;
-        int n = nums.size();
-        if(nums[0] == 1) return 0;
-        for(int i = 1 ; i< n ; i++){
-            diff = nums[i] - nums[i-1];
-            if(diff != 1) {
-                ans = nums[i-1] + 1;
-                return ans;
-            }
+    //     sort(nums.begin() , nums.end());
+    //     int ans = -1;
+    //     int diff = 1;
+    //     int n = nums.size();
+    //     if(nums[0] == 1) return 0;
+    //     for(int i = 1 ; i< n ; i++){
+    //         diff = nums[i] - nums[i-1];
+    //         if(diff != 1) {
+    //             ans = nums[i-1] + 1;
+    //             return ans;
+    //         }
+    //     }
+
+    //     return nums[n-1] + 1;
+    // }
+
+    int optimal_sol1(vector<int>&nums){
+        int ans=0 ;
+        
+        for(int i =0; i<nums.size(); i++){
+            ans =  ans ^ nums[i];
         }
 
-        return nums[n-1] + 1;
+        for(int i =0; i<=nums.size(); i++){
+            ans =  ans ^ i;
+        }
+        return ans;
     }
-
-    // int optimal_sol1(vector<int>&nums){
-    //     int ans=0 ;
-        
-    //     for(int i =0; i<nums.size(); i++){
-    //         ans =  ans ^ nums[i];
-    //     }
-
-    //     for(int i =0; i<=nums.size(); i++){
-    //         ans =  ans ^ i;
-    //     }
-    //     return ans;
-    // }
 
     int missingNumber(vector<int>& nums) {
 
     //  return bruteForce_sol(nums);
-    return brute_force2(nums);
-    // return optimal_sol1(nums);
+    // return brute_force2(nums);
+    return optimal_sol1(nums);
 
         // int sum = 0;
         // int n = nums.size();
