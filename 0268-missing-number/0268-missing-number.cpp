c91cd73502f54ad6e0530dcaed696adf29cc1f91
Aditya yadav <157573310+Aadityaa-11@users.noreply.github.com>
2025-11-15 19:00:33 +0530

    Time: 0 ms (100%), Space: 21.9 MB (37.55%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,14 +1,32 @@
 class Solution {
 public:
 
-    int bruteForce_sol(vector<int>&nums){
+    // int bruteForce_sol(vector<int>&nums){
+    //     sort(nums.begin() , nums.end());
+    //     for(int i =0; i<nums.size(); i++){
+    //         if(i == nums[i])continue;
+    //         else 
+    //         return i;
+    //     }
+    //     return nums.size();
+    // }
+
+    int brute_force2(vector<int>& nums){
+
         sort(nums.begin() , nums.end());
-        for(int i =0; i<nums.size(); i++){
-            if(i == nums[i])continue;
-            else 
-            return i;
+        int ans = -1;
+        int diff = 1;
+        int n = nums.size();
+        if(nums[0] == 1) return 0;
+        for(int i = 1 ; i< n ; i++){
+            diff = nums[i] - nums[i-1];
+            if(diff != 1) {
+                ans = nums[i-1] + 1;
+                return ans;
+            }
         }
-        return nums.size();
+
+        return nums[n-1] + 1;
     }
 
     // int optimal_sol1(vector<int>&nums){
@@ -26,7 +44,8 @@ public:
 
     int missingNumber(vector<int>& nums) {
 
-     return bruteForce_sol(nums);
+    //  return bruteForce_sol(nums);
+    return brute_force2(nums);
     // return optimal_sol1(nums);
 
         // int sum = 0;
