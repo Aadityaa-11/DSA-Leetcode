c0fd80207118be95973187128f8d3786d1081bb1
Aditya yadav <157573310+Aadityaa-11@users.noreply.github.com>
2025-11-16 18:41:19 +0530

    Time: 0 ms (100%), Space: 11.3 MB (44.75%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -23,10 +23,9 @@ public:
             if( nums[i] > nums[(i+1) % n]){
                  count++;
             }
-            if(count > 1) return false;
-
         }
-        
-         return true;   
+        if(count > 1) return false;
+        else
+        return true;   
     }
 };
