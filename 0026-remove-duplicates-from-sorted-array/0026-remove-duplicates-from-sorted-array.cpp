f79cee11ae2fc90c001e4c13a24843b4cbdb030d
Aditya yadav <157573310+Aadityaa-11@users.noreply.github.com>
2025-11-16 17:49:57 +0530

    Time: 0 ms (100%), Space: 22.8 MB (19.08%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int brute_force(vector<int>& nums){
+        vector<int> temp;
+
+        for(auto x : nums){
+            if(temp.empty() || temp.back() != x){
+                temp.push_back(x);
+            }
+        }
+
+        for(int i = 0 ; i<temp.size() ; i++){
+            nums[i] = temp[i];
+        }
+        return temp.size();
+
+    }
+    int removeDuplicates(vector<int>& nums) { 
+
+        int ans = brute_force(nums);
+        return ans;
+
+        // // TWO POINTER APPROACH 
+        // int j = 0; // j is the unique elements first element of array is unique 
+        // // so i start from 1
+
+        // for(int i = 1; i < nums.size(); i++){
+        //     if(nums[i] != nums[j]){
+        //          // different wala case
+        //         // pahele j ko aage badao phir different element that lie on i index put on j index
+        //         j++;
+        //         nums[j] = nums[i];
+        //     }
+        
+        // }
+
+        // // j is index of unique (0-based indexing)
+        // // int k = j+1;
+        // // return k;
+        // return j+1;
+    }
+};
